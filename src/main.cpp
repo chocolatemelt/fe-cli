5f5d0bbd2bd3bbc62b198937ed96544401d865fa
Kevin Zhang <kzhaaang@gmail.com>
2015-08-22 01:45:30 -0400

    add feRNG test

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,16 @@
 
 #include "feCharacter.h"
 #include "feClass.h"
+#include "feUtils.h"
 
 int main() {
   feClass c;
   feCharacter test('m', "My Unit", false, 'p', c);
+  feUtils utils;
   initscr();
   printw("initialization of character MU successful\n");
+  int a = utils.feRNG();
+  printw("%d\n", a);
   printw("testing terminal capabilities:\n");
   if(has_colors()) printw("terminal supports colors\n");
   else printw("terminal does not support colors\n");
