f5ea109952a4350d5409cc040726873c29603928
Kevin Zhang <kzhaaang@gmail.com>
2015-08-22 01:44:56 -0400

    add feRNG function

--- src/feUtils.h
+++ src/feUtils.h
@@ -9,14 +9,11 @@ class feUtils {
   public:
     std::mt19937 rand;
 
-    feUtils() {
-      rand.seed(time(NULL));
-    }
+    feUtils();
     
-    feUtils(std::string s) {
-      std::seed_seq seed(s.begin(), s.end());
-      rand.seed(seed);
-    }
+    feUtils(std::string s);
+
+    int feRNG();
 };
 
 #endif
