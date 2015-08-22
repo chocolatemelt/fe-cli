7a0caf72b181f235a8803d6ee39353c3c6da8f38
Kevin Zhang <kzhaaang@gmail.com>
2015-08-22 02:26:24 -0400

    add main test comments

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,32 @@
 #include "feUtils.h"
 
 int main() {
+  // initialize
   feClass c;
   feCharacter test('m', "My Unit", false, 'p', c);
   feUtils utils;
   initscr();
+
+  // test character and class initialization
   printw("initialization of character MU successful\n");
-  int a = utils.feRNG();
-  printw("%d\n", a);
+
+  // test RNG functionality
+  printw("testing RNG machine\n");
+  int a;
+  for(int i = 0; i < 100; ++i) {
+    a = utils.feRNG();
+    printw("%d ", a);
+  }
+  printw("\nRNG execution successful\n");
+
+  // test terminal capabilities
   printw("testing terminal capabilities:\n");
   if(has_colors()) printw("terminal supports colors\n");
   else printw("terminal does not support colors\n");
   if(can_change_color()) printw("terminal can change colors\n");
   else printw("terminal cannot change colors\n");
+
+  // get char to keep window open, and exit
   getch();
   endwin();
   return 0;
