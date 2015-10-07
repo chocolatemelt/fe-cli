abd5839976fb63cf307c17f2da092ff0b7ffb28e
sihsob <bcsihsobhon@gmail.com>
2015-10-06 21:44:40 -0400

    Fixed commit with added utilities

--- src/feUtils.cpp
+++ src/feUtils.cpp
@@ -11,6 +11,7 @@
  * Return: Nothing
  */
 void feUtils::setAttack(const std::vector<int>& character_stats) {
+    attack = 0;
     std::vector<int>::const_iterator itr = character_stats.begin();
     for( ; itr != character_stats.end(); ++itr)
         attack += *itr;
@@ -21,13 +22,19 @@ void feUtils::setAttack(const std::vector<int>& character_stats) {
  *    weapon_stats: vector of ints containing
  *    values of the necessary character/weapon stats needed
  *    for calculating the actual hit rate of a character
+ *    rank: a character representing a character's 
+ *    weapon rank (capital letter)
+ *    buffOrDebuff: an integer representing whether it's 
+ *    debuff or buff to character
  *
  * Return: Nothing
  */
-void feUtils::setHitRate(const std::vector<int>& weapon_stats) {
+void feUtils::setHitRate(const std::vector<int>& weapon_stats, const char rank, const int buffOrDebuff) {
+    hit_rate = 0;
     hit_rate = weapon_stats[0] +
                ((weapon_stats[1] * 3) + ceil(weapon_stats[2])/2) +
                weapon_stats[4];
+    weaponTriangle(rank, buffOrDebuff);
 }
 
 /**
@@ -39,6 +46,7 @@ void feUtils::setHitRate(const std::vector<int>& weapon_stats) {
  * Return: Nothing
  */
 void feUtils::setCritical(const std::vector<int>& character_stats) {
+    critical = 0;
     critical = character_stats[0] + ceil(character_stats[1]/2);
 }
 
@@ -51,6 +59,7 @@ void feUtils::setCritical(const std::vector<int>& character_stats) {
  * Return: Nothing
  */
 void feUtils::setAvoid(const std::vector<int>& character_stats) {
+    avoid = 0;
     avoid = ceil(((character_stats[0] * 3) + character_stats[1])/2);
 }
 
@@ -63,6 +72,7 @@ void feUtils::setAvoid(const std::vector<int>& character_stats) {
  * Return: Nothing
  */
 void feUtils::setRating(const std::vector<int>& character_stats) {
+    rating = 0;
     std::vector<int>::const_iterator itr = character_stats.begin();
     for( ; itr != character_stats.end(); ++itr)
         rating += *itr;
@@ -79,11 +89,62 @@ void feUtils::setRating(const std::vector<int>& character_stats) {
  * resets the specified stat to 0 to calculate, since most likely
  * calling that method because it needs to be reset anyways.  That
  * way can avoid having to reset everything at once.
- */
+ *//*
 void feUtils::resetCombatStats() {
     attack = 0;
     hit_rate = 0;
     critical = 0;
     avoid = 0;
     rating = 0;
+}*/
+
+/**
+ * Parameters: 
+ *      rank: a character representing a character's weapon rank (capital letter)
+ *      buffOrDebuff: an integer representing whether it's debuff or buff to character
+ *
+ * Modifies: Sets all the combat stats back to zero.  Use when a
+ * character level ups or changes weapons, so that the proper values
+ * can be recalculated
+ *
+ * Notes: Might not be needed if at the start of every "set" method,
+ * resets the specified stat to 0 to calculate, since most likely
+ * calling that method because it needs to be reset anyways.  That
+ * way can avoid having to reset everything at once.
+ */
+int weaponTriangle(const char rank, const int buffOrDebuff) {
+    // Buff
+    if(buffOrDebuff == 1) {
+        if(rank == 'E' || rank == 'D') 
+            hit_rate += 5;
+        else if(rank == 'C')
+            hit_rate += 10;
+        else if(rank == 'B')
+        {
+            hit_rate += 10;
+            attack += 1;
+        }
+        else if(rank == 'A')
+        {
+            hit_rate += 15;
+            attack += 1;
+        }
+    }
+    // Debuff
+    else {
+        if(rank == 'E' || rank == 'D') 
+            hit_rate -= 5;
+        else if(rank == 'C')
+            hit_rate -= 10;
+        else if(rank == 'B')
+        {
+            hit_rate -= 10;
+            attack +-= 1;
+        }
+        else if(rank == 'A')
+        {
+            hit_rate -= 15;
+            attack -= 1;
+        }
+    }
 }
