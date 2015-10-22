8bbbc44458f5d592c2f54e8be0e6794405a5bc3a
Kevin Zhang <zhangk6@rpi.edu>
2015-10-22 16:55:30 -0400

    rng test

--- src/tests.cpp
+++ src/tests.cpp
@@ -11,21 +11,21 @@ int main() {
   feCharacter test('m', "Marth", true, 'p', c);
   feUtils utils;
 
-  //printw("initialization of character MU successful\n");
+	// test character initialization
   if (test.printInfo() == "mMarthmalep") {
     std::cout << ("1");
   }
   else std::cout << (test.printInfo());
-  //printw("testing terminal capabilities:\n");
 
   // test RNG functionality
+	// not easy to test randomness, but we can check if we're creating numbers
   std::vector<int> test_vector;
   int a;
-  for(int i = 0; i < 100; ++i) {
+  for(int i = 0; i < 10000; ++i) {
     a = utils.feRNG();
-    test_vector.push_back(a);
+		if(a >= 0 && a <= 99) test_vector.push_back(a);
   }
-  if (test_vector.size() == 100) std::cout << ("1");
+  if (test_vector.size() == 10000) std::cout << ("1");
   else std::cout << ("0");
 
   std::cout << std::endl;
