b0e01687869dcb7dc8ed1e05a25cad408311765e
Kevin Zhang <zhangk6@rpi.edu>
2015-10-26 15:02:19 -0400

    add menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,37 @@
+#include <cstdlib>
 #include <fstream>
 #include <string>
 #include <ncurses.h>
+#include <menu.h>
 
 #include "feCharacter.h"
 #include "feClass.h"
 #include "feUtils.h"
+
+// lazy af
+#define ARRAY_SIZE(array) (sizeof((array))/sizeof((array[0])))
 
 std::string get_file_contents(const char *);
 
 int main() {
   // initialize
   initscr();
+	cbreak();
+	noecho();
+	keypad(stdscr, true);
+
 	int row, col, dispr, dispc;
 	std::string logopath = "dat/logo_jp.txt";
+
+	const char *menu[] = {
+		"Start",
+		"Rosters",
+		"Options",
+		"Exit"
+	};
+	ITEM **items;
+	ITEM *cur_item;
+	MENU *game_menu;
 
 	// get row, column, and displacements
 	getmaxyx(stdscr,row,col);
@@ -29,12 +48,38 @@ int main() {
 	}
 	refresh();
 
-	// get char to keep window open, and exit
-  getch();
+	// build menu (looks more c than c++ to me...)
+	int c, n_choices;
+	n_choices = ARRAY_SIZE(menu);
+	items = (ITEM **) calloc(n_choices + 1, sizeof(ITEM *));
+	for(int i = 0; i < n_choices; ++i) items[i] = new_item(menu[i], menu[i]);
+	items[n_choices] = (ITEM *) NULL;
+	game_menu = new_menu((ITEM **) items);
+	post_menu(game_menu);
+
+	// exit handler
+	while((c = getch()) != 10){
+		switch(c) {
+			case KEY_DOWN:
+		    menu_driver(game_menu, REQ_DOWN_ITEM);
+				break;
+			case KEY_UP:
+				menu_driver(game_menu, REQ_UP_ITEM);
+				break;
+		}
+	}
+
+	// free stuff up and exit
+	free_item(items[0]);
+	free_item(items[1]);
+	free_menu(game_menu);
   endwin();
   return 0;
 }
 
+/**
+ * store file contents into a string
+ */
 std::string get_file_contents(const char *filename) {
   std::ifstream in(filename, std::ios::in | std::ios::binary);
   if (in)
