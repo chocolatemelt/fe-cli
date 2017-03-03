0a353a03958eeb83fc73972bde635ed08c6ed0da
Kevin Zhang <kzhaaang@gmail.com>
2017-03-03 11:48:01 -0500

    remove comments

--- src/feClass.h
+++ src/feClass.h
@@ -8,12 +8,6 @@
 #include "feSkill.h"
 #include "feStats.h"
 
-/**
- * Barebones class: will read in data about each class into a map and store in
- * memory at the start as a map of classes.  Since each character's official
- * stats must be taken from a conglomeration of class stats and base stats,
- * have characters define the getters/setters of their stats.
- */
 class feClass {
 private:
     std::string name;
