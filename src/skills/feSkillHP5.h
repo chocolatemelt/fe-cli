fd881b31f67c8ff413d843f7ef0df2baa663338b
Kevin Zhang <kzhaaang@gmail.com>
2017-03-03 12:38:14 -0500

    minor fix, implementing singleton

--- src/skills/feSkillHP5.h
+++ src/skills/feSkillHP5.h
@@ -4,9 +4,16 @@
 #include "feSkillStat.h"
 
 class feSkillHP5 : public feSkillStat {
+	private:
+		static feSkillHP5 *instance;
+		feSkillHP5() {}
 	public:
+		static feSkillHP5 *self() {
+			if(!instance) instance = new feSkillHP5;
+			return instance;
+		}
 		feStats apply() {
-			return feStats(5, 0, 0, 0, 0, 0, 0, 0, 0, 0,);
+			return feStats(5, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 		}
 };
 
