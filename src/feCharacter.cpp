941a89edc179975c037f70f4230454d5d7493f00
Kevin Zhang <kzhaaang@gmail.com>
2017-03-03 11:38:21 -0500

    fix initialization list

--- src/feCharacter.cpp
+++ src/feCharacter.cpp
@@ -1,51 +1,49 @@
 #include "feCharacter.h"
 
-feCharacter::feCharacter(std::string id, std::string n, bool g, char l, char a, feClass j) {
-  uniqID = id;
-  name = n;
-  gender = g;
-  loyalty = l;
-  affinity = a;
-  job = j;
-  weaponRank = feWeaponRank(j);
+feCharacter::feCharacter(std::string id, std::string n, bool g, char l, char a, feClass j)
+	: weaponRank(j) {
+		uniqID = id;
+		name = n;
+		gender = g;
+		loyalty = l;
+		affinity = a;
+		job = j;
 
-  /* initialize stats */
-  initStats(j);
-}
+		/* initialize stats */
+		initStats(j);
+	}
 
 /**
  * zeroStats()
  * Initializes stats (does nothing for stats otherwise).
  */
 void feCharacter::initStats(feClass c) {
-    feStats classbase = c.getBaseStats();
-    base = classbase;
-    current = base;
-    bonus = base;
+	feStats classbase = c.getBaseStats();
+	base = classbase;
+	current = base;
+	bonus = base;
 
-    feStats classgrowth = c.getGrowthStats();
-    growth = classgrowth;
-    feStats classcap = c.getCapStats();
-    cap = classcap;
+	feStats classgrowth = c.getGrowthStats();
+	growth = classgrowth;
+	feStats classcap = c.getCapStats();
+	cap = classcap;
 }
 
 std::string feCharacter::printInfo() {
 
-  std::stringstream ss;
-  std::string id;
-  ss << uniqID;
-  ss >> id;
+	std::stringstream ss;
+	std::string id;
+	ss << uniqID;
+	ss >> id;
 
-  std::stringstream ll;
-  std::string loy;
-  ll << loyalty;
-  ll >> loy;
+	std::stringstream ll;
+	std::string loy;
+	ll << loyalty;
+	ll >> loy;
 
-  std::string g;
-  if (gender == 0) g = "female";
-  if (gender == 1) g = "male";
+	std::string g;
+	if (gender == 0) g = "female";
+	if (gender == 1) g = "male";
 
-
-
-  return id + name + g + loy;
+	return id + name + g + loy;
 }
