22ecd098468650c6accd925c21c44b217905ee6c
Ben Hu <BenjiHu@users.noreply.github.com>
2017-03-21 17:38:53 -0400

    removes feSkill get()

--- src/feSkill.cpp
+++ src/feSkill.cpp
@@ -1,10 +1,1 @@
 #include "feSkill.h"
-
-feSkill get(std::string skill) {
-	switch(skill) {
-		case "HP+5":
-			return feSkillHP5::self();
-		default:
-			return feSkillHP5::self(); // this is undefined behavior... should throw an error
-	}
-}
