2147d0da2f210e1646296e4ce29d577fcce481e5
Kevin Zhang <kzhaaang@gmail.com>
2017-04-04 03:38:45 -0400

    quick outlines

--- src/feJsonUtils.h
+++ src/feJsonUtils.h
@@ -1,7 +1,19 @@
 #ifndef JSONUTILS_H
 #define JSONUTILS_H
 
+#include <string>
+
+#include "feCharacter.h"
+#include "feClass.h"
+
+#include "lib/json.hpp"
+
+using json = nlohmann::json;
+
 public class feJsonUtils {
+	public:
+		static feCharacter *importCharacter(std::string characterJson);
+		static feClass *importClass(std::string classJson);
 };
 
 #endif
