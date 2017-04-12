30416f1f5b4113577a3286addfb66e500687a740
Ben Hu <benjaminhu14928@outlook.com>
2017-04-12 16:02:28 -0400

    JSON tools
    adds function to turn feClass into JSON using nlohmann
    doesnt add items currently

---
+++ src/feJsonUtils.cpp
@@ -0,0 +1,44 @@
+/*
+ * feJsonUtils.cpp
+ *
+ * implementations to turn feClass, feChar, and feItem into JSON
+ * uses nlohmann's JSON header file
+ */
+
+#include "feJsonUtils.h"
+
+static std::vector<int> stattovector(feStats stats) {
+	std::vector<int> ret;
+	ret.push_back(stats.hp);
+	ret.push_back(stats.str);
+	ret.push_back(stats.mag);
+	ret.push_back(stats.skl);
+	ret.push_back(stats.spd);
+	ret.push_back(stats.lck);
+	ret.push_back(stats.def);
+	ret.push_back(stats.res);
+	ret.push_back(stats.con);
+	ret.push_back(stats.mov);
+
+	return ret;
+}
+
+static json feJsonUtils::exportClass(feClass cls) {
+	json jsonClass;
+
+	jsonClass["name"] = cls.getClassName();
+
+	std::vector<int> basestat = stattovector(cls.getBaseStats());
+	jsonClass["base"] = basestat;
+
+	std::vector<int> capstat = stattovector(cls.getCapStats());
+	jsonClass["cap"] = capstat;
+
+	std::vector<int> growthstat = stattovector(cls.getGrowthStats());
+	jsonClass["growth"];
+
+	jsonClass["weaponType"] = cls.getWeaponTypes();
+	jsonClass["promotion"] = cls.getPromotions();
+
+	return jsonClass;
+}
--- src/feJsonUtils.h
+++ src/feJsonUtils.h
@@ -5,24 +5,25 @@
 
 #include "feCharacter.h"
 #include "feClass.h"
+#include "feItem.h"
 
 #include "lib/json.hpp"
 
 using json = nlohmann::json;
 
-public class feJsonUtils {
+class feJsonUtils {
 	public:
 		static json exportCharacter(feCharacter chr);
 		static json exportClass(feClass cls);
-		static json exportItem(feItem itm);
+		//static json exportItem(feItem itm);
 
 		static feCharacter *importCharacter(json characterJson);
 		static feClass *importClass(json classJson);
-		static feItem *importItem(json itemJson);
+		//static feItem *importItem(json itemJson);
 
 		static feCharacter *importCharacterFromString(std::string characterJsonStr);
 		static feClass *importClassFromString(std::string classJsonStr);
-		static feItem *importItemFromString(std::string itemJsonStr);
+		//static feItem *importItemFromString(std::string itemJsonStr);
 };
 
 #endif
