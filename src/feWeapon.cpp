a95df97735cbc8e80bcc6b4c977c85dce7daa2c9
Kevin Zhang <kzhaaang@gmail.com>
2017-04-21 01:44:10 -0400

    update weapon class definitions

--- src/feWeapon.cpp
+++ src/feWeapon.cpp
@@ -1,1 +1,9 @@
 #include <feWeapon.h>
+
+feWeapon::feWeapon() {
+	might = 0;
+	hitrate = 0;
+	critical = 0;
+	physical = true;
+	type = "none";
+}
--- src/feWeapon.h
+++ src/feWeapon.h
@@ -1,6 +1,8 @@
 #ifndef WEAPON_H
 #define WEAPON_H
 
+#include <string>
+
 #include "feItem.h"
 
 class feWeapon : public feItem  {
@@ -8,11 +10,16 @@ class feWeapon : public feItem  {
 		int might;
 		int hitrate;
 		int critical;
+		bool physical;
+		std::string type;
 
 	public:
-		int getMt() { return might; }
-		int getHr() { return hitrate; }
-		int getCrit() { return critical; }
+		feWeapon();
+		int getCritical() const { return critical; }
+		int getHitrate() const { return hitrate; }
+		int getMight() const { return might; }
+		int isPhysical() const { return physical; }
+		std::string getType() const { return type; }
 };
 
 #endif
