8d827239f2011a5368f263d58d11007e2b191ad1
Kevin Zhang <kzhaaang@gmail.com>
2017-04-25 23:39:16 -0400

    utility tests

--- tests/utils.cpp
+++ tests/utils.cpp
@@ -1,4 +1,18 @@
-//
-// Created by Kevin on 4/25/2017.
-//
+/**
+ * tests/utils.cpp
+ * Generic tests for the feUtils class.
+ */
 
+#include "catch.hpp"
+
+#include "feUtils.h"
+
+TEST_CASE("RNG correctly outputs 0-99", "[feUtils]") {
+	std::vector<int> testVector;
+	int a;
+	for(int i = 0; i < 10000; ++i) {
+		a = utils.feRNG();
+		if(a >= 0 && a <= 99) testVector.push_back(a);
+	}
+	REQUIRE(testVector.size() == 10000);
+}
