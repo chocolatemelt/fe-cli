7794f8ef274ab406d5f12fa9d85a61001bdb8c19
Kevin Zhang <kzhaaang@gmail.com>
2017-04-26 01:31:42 -0400

    distribution output for humans to check

--- tests/utils.cpp
+++ tests/utils.cpp
@@ -3,15 +3,20 @@
  * Generic tests for the feUtils class.
  */
 
+#include <iostream>
 #include "catch.hpp"
 
 #include "feUtils.h"
 
 TEST_CASE("RNG correctly outputs 0-99", "[feUtils]") {
+	feUtils utils;
 	std::vector<int> testVector;
+
+	std::cout << "[feUtils]: Check distribution:" << std::endl;
 	int a;
 	for(int i = 0; i < 10000; ++i) {
 		a = utils.feRNG();
+		std::cout << a << " ";
 		if(a >= 0 && a <= 99) testVector.push_back(a);
 	}
 	REQUIRE(testVector.size() == 10000);
