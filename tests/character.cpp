d01ce589a94fa2532d61b17384bda074b6a2128d
Kevin Zhang <kzhaaang@gmail.com>
2017-04-25 22:16:42 -0400

    update style

---
+++ tests/character.cpp
@@ -0,0 +1,9 @@
+/**
+ * tests/character.cpp
+ * Generic tests for the feCharacter class.
+ */
+
+#include "catch.hpp"
+
+#include "feCharacter.h"
+
---
+++ tests/tests.cpp
@@ -0,0 +1,9 @@
+/**
+ * tests/tests.cpp
+ * This is just the main tests file for Catch!
+ * !! DO NOT WRITE TEST CASES IN THIS FILE !!
+ * See https://github.com/philsquared/Catch/blob/master/docs/slow-compiles.md for more on the subject.
+ */
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
