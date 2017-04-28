20ac8aedf15df20cca13199e1ff530efbea34c84
Kevin Zhang <kzhaaang@gmail.com>
2017-04-28 16:37:17 -0400

    rearrange to better fit catch system

--- tests/mechanics.cpp
+++ tests/mechanics.cpp
@@ -7,20 +7,32 @@
 
 #include "feMechanics.h"
 
-TEST_CASE("Weapon gets assigned correct color", "[feMechanics]") {
-	feWeapon sword(0, 90, 12, true, "swoerd", "Silver Sword");
-	feWeapon axe(0, 70, 16, true, "axe", "Silver Axe");
-	feWeapon lance(0, 80, 14, true, "lance", "Silver Lance");
-	CHECK(feMechanics::getWeaponColor(sword) == 'r');
-	CHECK(feMechanics::getWeaponColor(axe) == 'g');
-	CHECK(feMechanics::getWeaponColor(lance) == 'b');
-}
-
-TEST_CASE("Weapon triangle correctly trumps", "[feMechanics]") {
+TEST_CASE("Weapon triangle", "[feMechanics]") {
 	feWeapon sword(0, 90, 12, true, "sword", "Silver Sword");
 	feWeapon axe(0, 70, 16, true, "axe", "Silver Axe");
 	feWeapon lance(0, 80, 14, true, "lance", "Silver Lance");
-	CHECK(feMechanics::weaponTriangle(sword, axe) == 1);
-	CHECK(feMechanics::weaponTriangle(axe, lance) == 1);
-	CHECK(feMechanics::weaponTriangle(lance, sword) == 1);
+
+	SECTION("Weapon gets assigned correct color") {
+		CHECK(feMechanics::getWeaponColor(sword) == 'r');
+		CHECK(feMechanics::getWeaponColor(axe) == 'g');
+		CHECK(feMechanics::getWeaponColor(lance) == 'b');
+	}
+
+	SECTION("Weapon triangle correctly trumps") {
+		CHECK(feMechanics::weaponTriangle(sword, axe) == 1);
+		CHECK(feMechanics::weaponTriangle(axe, lance) == 1);
+		CHECK(feMechanics::weaponTriangle(lance, sword) == 1);
+	}
+
+	SECTION("Weapon triangle correctly fails") {
+		CHECK(feMechanics::weaponTriangle(sword, lance) == -1);
+		CHECK(feMechanics::weaponTriangle(axe, sword) == -1);
+		CHECK(feMechanics::weaponTriangle(lance, axe) == -1);
+	}
+
+	SECTION("Weapon triangle correctly returns 0") {
+		CHECK(feMechanics::weaponTriangle(sword, sword) == 0);
+		CHECK(feMechanics::weaponTriangle(axe, axe) == 0);
+		CHECK(feMechanics::weaponTriangle(lance, lance) == 0);
+	}
 }
